0fbf4376fe6c80185085db4af5def893e1387e7e
Ashir Imran <ashirimran@Ashirs-MacBook-Pro.local>
2021-01-07 23:12:56 -0500

    Updates 2

--- commands/test.js
+++ commands/test.js
@@ -4,8 +4,7 @@ module.exports = {
     execute(msg) {
         const prefix = "!";
         if(msg.content[0] === prefix) {
-            const args = msg.content.substring(msg.content.indexOf(" ") + 1, msg.content.length);
-            msg.channel.send(`${args}`);
+            msg.channel.send("Testing!");
         }
     }
 }
